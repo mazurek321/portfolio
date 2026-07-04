b3c5155a703c55166038a20f017f27e85fc96efb
mazurek321 <mazurek321.93@wp.pl>
2026-07-04 12:28:03 +0200

    Portfolio update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
         {/* {loading && <WelcomeLoader/>} */}
-        {/* <WelcomeLoader/> */}
+        <WelcomeLoader/>
     
         <Navbar/>
         <main>
